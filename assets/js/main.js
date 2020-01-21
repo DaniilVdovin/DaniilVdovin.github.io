fe84c66df5d4a77da0d088dac970f7cd3333eab8
MikeMarelo <stels1040533@gmail.com>
2020-01-21 20:48:02 +0300

    0.402

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,7 +213,10 @@
 					}
 
 					$t._update();
-
+                    
+                    reelWidth = $reel[0].scrollWidth;
+                    $t._update();
+                    
 					$window.on('resize', function() {
 						reelWidth = $reel[0].scrollWidth;
 						$t._update();
